9014bd7087c0bb30e19177fb29891b1c05fb361d
Md. Minul Islam Shovon <mainulislam588@gmail.com>
2020-10-06 22:55:51 +0600

    Adding UserProfile model

--- auction/models.py
+++ auction/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+from phonenumber_field.modelfields import PhoneNumberField
 
 # Create your models here.
+
+
+class UserProfile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=64, blank=True, null=True)
+    last_name = models.CharField(max_length=64, blank=True, null=True)
+    address = models.CharField(max_length=64, blank=True, null=True)
+    phone = PhoneNumberField(blank=True, null=True)
+    profile_pic = models.ImageField(upload_to='profile_pic', blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
+
+
