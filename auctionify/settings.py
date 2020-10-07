6ac4d374a1528be966771830ea7ce113e3f63db8
Md. Minul Islam Shovon <mainulislam588@gmail.com>
2020-10-07 22:12:26 +0600

    Adding media and static path in settings file

--- auctionify/settings.py
+++ auctionify/settings.py
@@ -119,4 +119,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / 'static'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "/static/"
+]
